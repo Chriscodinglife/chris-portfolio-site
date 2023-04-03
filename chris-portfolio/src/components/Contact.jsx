b4d4039e2a5f88a6ced5a7416be688646cde706a
Chriscodinglife <corella4@pratt.edu>
2023-04-02 20:05:37 -0400

    changed the env variable

--- chris-portfolio/src/components/Contact.jsx
+++ chris-portfolio/src/components/Contact.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const messageURL = "https://getform.io/f/777b09ee-e3d1-4e71-82e2-a7e15cf873df";
+const messageURL = process.env.MESSAGEURL;
 
 const Contact = () => {
   return (
