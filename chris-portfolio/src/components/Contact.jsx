1411505e0a19db53d034482b19f087124103782f
Chriscodinglife <corella4@pratt.edu>
2023-04-02 20:40:41 -0400

    headache

--- chris-portfolio/src/components/Contact.jsx
+++ chris-portfolio/src/components/Contact.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 
-const { message } = import.meta.env.VITE_MESSAGE;
-
 const Contact = () => {
   return (
     <div id="contact" className="max-w-[1040px] m-auto md:pl-20 p-4 py-16">
       <h1 className="py-4 text-4xl font-bold text-center text-[#001b5e]">
         Contact
       </h1>
-      <form action={message} method="POST" encType="multipart/form-data">
+      <form
+        action={__MESSAGE_ENV__}
+        method="POST"
+        encType="multipart/form-data"
+      >
         <div className="grid md:grid-cols-2 gap-4 w-full py-2">
           <div className="flex flex-col">
             <label className="uppercase text-sm py-2">Name</label>
