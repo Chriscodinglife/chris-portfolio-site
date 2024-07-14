4de045bfd94640033c050aba43a25b4196d44188
Chris Orellana <chrisorellana@chriss-mbp.mynetworksettings.com>
2024-07-14 17:31:40 -0400

    freshening up

---
+++ chris-portfolio/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <div className="bg-gray-800">
+            <div className="h-auto m-auto w-auto">
+                <div className="flex flex-col justify-center">
+                    <p className=" text-white text-center m-16 font-monts ">© Chris Orellana 2024</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Footer;
--- chris-portfolio/src/components/Sidenav.jsx
+++ chris-portfolio/src/components/Sidenav.jsx
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
 import {
-  AiOutlineHome,
   AiOutlineMenu,
   AiOutlineProject,
   AiOutlineMail,
+  AiFillHome,
+  AiFillFire,
+  AiFillExperiment,
+  AiFillMail,
 } from "react-icons/ai"; // This is for importing the react icons
-import { GrProjects } from "react-icons/gr"; // This is for importing the react icons
 import { BsPerson } from "react-icons/bs"; // This is for importing the react icons
 
 // This is the Side Nav bar that will be on the left side of the page
@@ -20,6 +22,16 @@ export const Sidenav = () => {
   // If nav is true, then show the nav bar
   // If nav is false, then don't show the nav bar
 
+
+  const iconSize = 20
+  const MyIcons = Object.freeze({
+    homeIcon: <AiFillHome size={iconSize} />,
+    workIcon: <AiFillFire size={iconSize} />,
+    projectIcon: <AiFillExperiment size={iconSize} />,
+    mailIcon: <AiFillMail size={iconSize} />
+  })
+
+
   return (
     <div>
       <AiOutlineMenu
@@ -28,12 +40,13 @@ export const Sidenav = () => {
       />
       {nav ? (
         <div className="fixed w-full h-screen bg-white/90 flex flex-col justify-center items-center z-20">
+          {/* This is for a mobile device */}
           <a
             onClick={handleNav}
             href="#main"
             className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
           >
-            <AiOutlineHome size={20} />
+            {MyIcons.homeIcon}
             <span className="pl-4">Home</span>
           </a>
           <a
@@ -41,7 +54,7 @@ export const Sidenav = () => {
             href="#work"
             className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
           >
-            <GrProjects size={20} />
+            {MyIcons.workIcon}
             <span className="pl-4">Work</span>
           </a>
           <a
@@ -49,60 +62,47 @@ export const Sidenav = () => {
             href="#projects"
             className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
           >
-            <AiOutlineProject size={20} />
+            {MyIcons.projectIcon}
             <span className="pl-4">Projects</span>
           </a>
-          {/* <a
-            onClick={handleNav}
-            href="#main"
-            className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
-          >
-            <BsPerson size={20} />
-            <span className="pl-4">Resume</span>
-          </a> */}
           <a
             onClick={handleNav}
             href="#contact"
             className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
           >
-            <AiOutlineMail size={20} />
+            {MyIcons.mailIcon}
             <span className="pl-4">Contact</span>
           </a>
         </div>
       ) : (
         ""
       )}
+      {/* This section is for a typical screen */}
       <div className="md:block hidden fixed top-[25%] z-10">
         <div className="flex flex-col">
           <a
             href="#main"
             className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pounter hover:scale-110 ease-in duration-300"
           >
-            <AiOutlineHome size={20} />
+            {MyIcons.homeIcon}
           </a>
           <a
             href="#work"
             className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pounter hover:scale-110 ease-in duration-300"
           >
-            <GrProjects size={20} />
+            {MyIcons.workIcon}
           </a>
           <a
             href="#projects"
             className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pounter hover:scale-110 ease-in duration-300"
           >
-            <AiOutlineProject size={20} />
+            {MyIcons.projectIcon}
           </a>
-          {/* <a
-            href="#main"
-            className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pounter hover:scale-110 ease-in duration-300"
-          >
-            <BsPerson size={20} />
-          </a> */}
           <a
             href="#contact"
             className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pounter hover:scale-110 ease-in duration-300"
           >
-            <AiOutlineMail size={20} />
+            {MyIcons.mailIcon}
           </a>
         </div>
       </div>
